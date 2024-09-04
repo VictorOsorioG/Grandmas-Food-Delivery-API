d67088a8c526693cac2b7eaa84658e83bbfd4310
JulianRgGlobant <julian.rg@globant.com>
2024-09-04 10:57:30 -0500

    Issue swager and add Readme

--- src/main/java/com/training/java/grandmassfood/delivery/api/controller/products/ProductController.java
+++ src/main/java/com/training/java/grandmassfood/delivery/api/controller/products/ProductController.java
@@ -3,6 +3,7 @@ package com.training.java.grandmassfood.delivery.api.controller.products;
 
 import com.training.java.grandmassfood.delivery.api.dao.products.dto.ProductGetResponse;
 import com.training.java.grandmassfood.delivery.api.service.products.ProductService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
+@Tag(name = "Product Controllers")
 public class ProductController {
     private final ProductService productService;
     @GetMapping("/{uuid}")
